f6c7ae75152f49bf76c92345de12f22e3c253aec
Gari Arellano <Gari01@github.com>
2021-09-18 13:35:51 +0200

    Añado un comentario

--- practica1/3-eco-numerico.cpp
+++ practica1/3-eco-numerico.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int main() {
     int numero, numero2;
 
-    cout << "Escriba dos números enteros separados por un espacio: ";
+    cout << "Escriba dos números enteros separados por un espacio: "; //solicito los numeros
     cin >> numero;
     cin >> numero2;
 
