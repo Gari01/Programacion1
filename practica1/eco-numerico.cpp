fa0fa3661198092344a27ef09785ac864b70974a
Gari Arellano <Gari01@github.com>
2021-09-19 17:14:37 +0200

    Nuevos archivos

---
+++ practica1/eco-numerico.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 1
+ * Autores: Miguel Ángel Latre
+ * Última revisión: 14 de septiembre de 2021
+ * Resumen: Programa que solicita un número entero al usuario y lo escribe en 
+ *          pantalla.
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Programa que solicita un número entero al usuario y lo escribe en pantalla.
+ */
+int main() {
+    int numero, numero2;
+
+    cout << "Escriba dos números enteros separados por un espacio: "; //solicito los numeros
+    cin >> numero;
+    cin >> numero2;
+
+    
+
+    cout << "La suma es " << numero + numero2 << endl;
+
+    return 0;
+}
