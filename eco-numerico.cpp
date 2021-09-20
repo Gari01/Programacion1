35f5ed35edd8f6e86d62b976fad7fca3cad586d8
Gari01 <64261417+Gari01@users.noreply.github.com>
2021-09-20 10:43:57 +0200

    Add files via upload

---
+++ eco-numerico.cpp
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Curso de Programación 1. Tema 1
+ * Autores: Miguel Ángel Latre
+ * Modificado por: Gari Arellano Zubía
+ * Última revisión: 14 de septiembre de 2021
+ * Resumen: Programa que solicita un número entero al usuario y lo escribe en 
+ *          pantalla.
+ ******************************************************************************/
+#include <iostream>
+using namespace std;
+
+/*
+ * Programa que solicita un número entero al usuario y lo escribe en pantalla.
+ */
+int main() {
+    int numero, numero2;
+
+    cout << "Escriba dos enteros separados por un espacio: "; //solicito los numeros
+    cin >> numero;
+    cin >> numero2;
+
+    
+
+    cout << "La suma es: " << numero + numero2 << endl;
+
+    return 0;
+}
